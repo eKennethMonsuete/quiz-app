57a2085de69ab2fd7e399ef8a04c68ee80de169f
Kenneth Monsuete <keeentt19@gmail.com>
2025-01-06 18:39:08 -0300

    projeto base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quiz-app';
+  title = 'quiz';
 }
--- src/app/simple-quiz/model/Question.ts
+++
@@ -1,6 +0,0 @@
-export interface Question {
-  enunciado: string;
-  alternatives: string[];
-  correctAnswer: number;
-  explanation: string;
-}
